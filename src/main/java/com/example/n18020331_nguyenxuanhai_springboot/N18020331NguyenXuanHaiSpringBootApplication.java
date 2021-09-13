358371b6cc43ec90858d3c54ccf8c354986d38a7
hainguyen2k <ngxuanhai1000@gmail.com>
2021-09-13 11:39:33 +0700

    Nop BT KTvTKPM Tuan1 SpringBoot

---
+++ src/main/java/com/example/n18020331_nguyenxuanhai_springboot/N18020331NguyenXuanHaiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.n18020331_nguyenxuanhai_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N18020331NguyenXuanHaiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(N18020331NguyenXuanHaiSpringBootApplication.class, args);
+    }
+
+}
